f7e41eb79558729ec58cb7f55b7246ab7a8e6478
JustBeYou <mihailferaru2000@gmail.com>
2018-07-12 17:52:11 +0300

    Implement few constructors and few helpers

--- vector/vector.h
+++ vector/vector.h
@@ -2,9 +2,30 @@
 #define VECTOR_H
 
 #include <cstddef>
+#include <cmath>
 #include <new>
 
 namespace selfmade {
+
+// I am not responsable for your integer overflows :P
+
+size_t helper_fastPow(size_t base, size_t exp) {
+    if (exp == 0) return 1;
+    if (exp == 1) return base;
+
+    if (exp % 2 == 0) return helper_fastPow(base * base, exp / 2);
+    return base * helper_fastPow(base * base, (exp - 1) / 2);
+}
+
+size_t helper_nearestTwoPow(size_t n) {
+    n--;
+    for (int i = 1; i < sizeof(size_t); i *= 2) {
+        n |= n >> i;
+    }
+    n++;
+
+    return n;
+}
 
 const std::size_t gMinVectorCapacity = 16;
 
@@ -16,19 +37,29 @@ class Vector {
         std::size_t mCapacity;
     public:
         Vector() {
-            
+           mVector = new type[gMinVectorCapacity];
+           mSize = 0;
+           mCapacity = gMinVectorCapacity;
         }
 
         Vector(const Vector<type>& aInstance) {
-
+            *this = aInstance;
         }
 
-        Vector<type>& operator=(const Vector<type>& aObject) {
-
+        Vector<type>& operator=(const Vector<type>& aInstance) {
+            release();
+            mCapacity = aInstance.capacity();
+            mSize = aInstance.size();
+            
+            mVector = new type[mCapacity];
+            for (int i = 0; i < mSize; i++) {
+                mVector[i] = aInstance[i];
+            }
+    
         }
 
         Vector(std::size_t aSize) {
-
+            
         }
 
         Vector(std::size_t aSize, const type* aArray) {
@@ -39,8 +70,17 @@ class Vector {
 
         }
 
-        ~Vector() {
+        void release() {
+            if (mVector != nullptr) {
+                delete[] mVector;
+                mVector = nullptr;
+                mSize = 0;
+                mCapacity = 0;
+            }
+        }
 
+        ~Vector() {
+            release();
         }
 
 
