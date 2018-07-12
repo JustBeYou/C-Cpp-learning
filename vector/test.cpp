b7ac30037fc2d06f27472f4c671dbe5d9780cca4
JustBeYou <mihailferaru2000@gmail.com>
2018-07-12 16:50:21 +0300

    Add gtest RUN_ALL_TESTS

--- vector/test.cpp
+++ vector/test.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 
 #include "vector.h"
+#include "gtest/gtest.h"
 
-using namespace std;
-
-int main() {
-
-    return 0;
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
