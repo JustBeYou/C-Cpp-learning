1475cc9f12dba058f41fbb2f4a9f5c0c8bceab15
Mihail Feraru <mihailferaru2000@gmail.com>
2020-05-31 09:05:18 +0300

    Create toptal_intwrview.cpp

---
+++ toptal_intwrview.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+/* Check if D is derived from B at compile-time */
+template<typename D, typename B>
+class IsDerivedFromHelper
+{
+    static constexpr bool Test( B* ) { return true; }
+    static constexpr bool Test( ... ) { return false; }
+public:
+    static constexpr bool Is = Test(static_cast<D*>(0));
+    
+};
+
+template <class C, class P> 
+bool IsDerivedFrom() {
+    return IsDerivedFromHelper<C, P>::Is;
+}
+
+
+/* Testing */
+class Base {};
+class Child: public Base {};
+class Test {};
+
+int main()
+{
+    cout << IsDerivedFrom<Child, Base>() << endl;
+    cout << IsDerivedFrom<Test, Base>() << endl;
+}
